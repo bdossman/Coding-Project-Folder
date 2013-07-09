43d76168e4bbbfcddcfedd8e06a757c2e5bbf39e
bdossman <dossman.2@osu.edu>
2013-07-09 12:34:08 -0400

    Added some files

---
+++ Localization/movement_model.R
@@ -0,0 +1,43 @@
+##############################################################################
+####				Simulating Simple Movement Model
+###############################################################################				
+
+##	Bryant Dossman July 5, 2012
+
+##	First attempt. Creating a simple fixed velocity and turning angle model...i.e straight line movement
+##	NOTE: To convert from slope(m) to angle (theta)----> theta = arctan(m)
+
+###############################################################################
+
+##	A simple linear movement model with constant velocity and direction in 
+##	cartesian coordinate system using fixed angle and fixed velocity.
+##	movement model --> S(t) = r(r,theta)
+
+##############################################################################
+
+## intial conditions
+
+rinit <- c(0,0)	# inital position
+h <- 1				# timestep
+n <- 1000			# number of locations
+
+
+## fixed parameters
+
+r <- 10			# magnitude of movement (velocity)
+theta <- pi/4	# equal to 45 degree angle
+
+## simulating movement path
+
+S <- matrix(0,nrow=n,ncol=2)
+S[1,] <- rinit
+
+for (i in 1:(n-1)){
+	S[i+1,] <- S[i,] + c(r*cos(theta), r*sin(theta))
+}
+
+###############################################################################
+
+## Using this simulated data create a dataset of radio hits on a collection of
+## towers.
+
--- Stopover_Behavior_Simulation.R
+++ Stopover_Behavior_Simulation.R
@@ -7,8 +7,8 @@
 
 time_steps <- 10 # number of days to run
 location_positions <- matrix(0,time_steps,2)
-habitat_matrix <- matrix(c(1,1,1,1,1, rep(0,20)),5,5,byrow=TRUE)
-
+habitat_matrix <- matrix(rep(1,10000),100,100,byrow=TRUE)
+M <- habitat_matrix
 ## function returns true false statement to inform whether a bird departs or not
 
 is_full <- function (R){
@@ -23,17 +23,33 @@ is_full <- function (R){
 condition_update <- function(R, location){
 	ifelse(M[location]==1,return(R+1), return(R))
 	# ifelse statement puts out vector of length 2 only the first value is returned
+	return(R)
 }
 
 ## Simulate movement steps and extract your position
 
 move <- function(current_location){
-	new_location <- current_location + c(rbinom(1,1,1),rbinom(1,1,1))
+	new_location <- current_location + c(rbinom(1,1,0.25),rbinom(1,1,0.75))
 	return(new_location)
 }
 
 
 
+####### Test Scenario
+location <- matrix(0,100,2)
+location[1,] <- c(25,13)
+R <- 0
+for (i in 1:100){
+	print(i)
+	## first check condition, STOP if condition max is reached
+	stopifnot(R < 1000)
+	## move to new location
+	location[i+1,] <- move(location[i,])
+	## update condition
+	R <- condition_update(R,location)
+	
+}
 
-
+l
+plot(location[,1],location[,2],type="l")
 
